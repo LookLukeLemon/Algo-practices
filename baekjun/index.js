8375110648db2ba4b5f356a47a3bfd11c05a8878
LookLukeLemon <looklukelemon@gmail.com>
2022-07-30 14:26:07 +0900

    10250

--- baekjun/index.js
+++ baekjun/index.js
@@ -4,27 +4,17 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 solution(fs.readFileSync(filePath).toString().split(EOL));
 
 function solution(input) {
-    const x = Number(input[0])
+    input.slice(1).map(i => {
+        const source = i.split(' ')
+        const h = Number(source[0])
+        const w = Number(source[1])
+        const n = Number(source[2])
 
-    let howMany = 1;
-    let nLine = 1;
+        if (1 <= h && w <= 99 && 1 <= n && n <= h * w) {
+            const line = Math.ceil(n / h)
+            const floor = n % h === 0 ? h : n % h
 
-    while (howMany < x) {
-        nLine += 1
-        howMany += nLine
-    }
-
-    if (nLine % 2 === 0) {
-        const offset = howMany - x
-        const maxRow = nLine - offset;
-        const maxCol = 1 + offset
-
-        console.log(`${maxRow}/${maxCol}`)
-    } else {
-        const offset = howMany - x
-        const maxRow = 1 + offset;
-        const maxCol = nLine - offset
-
-        console.log(`${maxRow}/${maxCol}`)
-    }
+            console.log(`${floor}${line.toString().padStart(2, 0)}`)
+        }
+    })
 }
